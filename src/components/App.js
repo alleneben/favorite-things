ec9addd18fd20fad73fdf5868b8928ac0bac5742
kod3180 <kod3180@gmail.com>
2019-11-13 00:30:23 +0000

    Routing set up

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,18 @@
 import React from "react";
 import { Provider } from "react-redux";
+import { BrowserRouter as Router } from "react-router-dom";
 import { configureStore } from "../redux";
+
+import Main from "./Main";
 
 const store = configureStore();
 
 const App = () => (
   <Provider store={store}>
-    
+    <Router>
+      {/* shared components goes here eg. navbar */} 
+      <Main />
+    </Router>
   </Provider>
 );
 
